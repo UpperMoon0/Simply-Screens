b7ef64d64816c1a431e9e4bc88a2447dfaa46eba
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2025-07-24 23:17:04 +0700

    feat: remove obsolete classes, assets, and add GitHub Actions release workflow

--- common/src/main/java/com/nstut/simplyscreens/creative_tabs/CreativeTabRegistries.java
+++ common/src/main/java/com/nstut/simplyscreens/creative_tabs/CreativeTabRegistries.java
@@ -2,7 +2,6 @@ package com.nstut.simplyscreens.creative_tabs;
 
 import com.nstut.simplyscreens.SimplyScreens;
 import com.nstut.simplyscreens.items.ItemRegistries;
-import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
@@ -13,12 +12,11 @@ import net.minecraft.world.item.ItemStack;
 public class CreativeTabRegistries {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(SimplyScreens.MOD_ID, Registries.CREATIVE_MODE_TAB);
 
-    public static final RegistrySupplier<CreativeModeTab> CREATIVE_TAB = CREATIVE_TABS.register(
-            "tab",
-            () -> CreativeTabRegistry.create(
-                    Component.translatable("itemGroup." + SimplyScreens.MOD_ID + ".tab"),
-                    () -> new ItemStack(ItemRegistries.SCREEN.get())
-            )
+    public static final RegistrySupplier<CreativeModeTab> CREATIVE_TAB = CREATIVE_TABS.register("tab", () -> CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
+            .title(Component.translatable("itemGroup." + SimplyScreens.MOD_ID + ".tab"))
+            .icon(() -> new ItemStack(ItemRegistries.SCREEN.get()))
+            .displayItems((params, output) -> ItemRegistries.ITEM_LIST.forEach(item -> output.accept(item.get())))
+            .build()
     );
 
     public static void init() {
